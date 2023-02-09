dc683775ec25bc5cee307a51f9ff2043ca934cf1
lucasedusi <lucasmto.16@hotmail.com>
2023-02-09 20:20:45 -0300

    :sparkles: feat: styling profile page

--- src/components/Repositories/styles.ts
+++ src/components/Repositories/styles.ts
@@ -12,8 +12,20 @@ export const Wrapper = styled.div`
 	border-bottom: 1px solid #e2e8f0;
 	margin-bottom: 1.2rem;
 
+	> h1 {
+		font-family: "Inter";
+		font-weight: 700;
+		font-size: 20px;
+		color: #171923;
+	}
+
 	> p {
 		margin: 1.2rem 0;
+		font-family: "Inter";
+		font-weight: 400;
+		font-size: 16px;
+		line-height: 24px;
+		color: #4a5568;
 	}
 `;
 
@@ -29,5 +41,21 @@ export const Footer = styled.div`
 		flex-direction: row;
 		align-items: center;
 		gap: 0.5rem;
+
+		> p {
+			font-family: "Inter";
+			font-weight: 400;
+			font-size: 14px;
+			line-height: 24px;
+			color: #4a5568;
+		}
+	}
+
+	> p {
+		font-family: "Inter";
+		font-weight: 400;
+		font-size: 14px;
+		line-height: 24px;
+		color: #4a5568;
 	}
 `;
