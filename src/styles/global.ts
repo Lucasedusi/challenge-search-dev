7dcbf3ba886be9298f1db855bf958401338b8732
lucasedusi <lucasmto.16@hotmail.com>
2023-02-09 09:47:41 -0300

    :sparkles: feat: config global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none;
+  }
+
+  body {
+    background: ${({ theme }) => theme.colors["base-background"]};
+    -webkit-font-smoothing: antialiased;
+    font-family: ${({ theme }) => theme.fonts.title};
+
+  }
+
+  body, input, textarea, button {
+    font-family: ${({ theme }) => theme.fonts.regular};
+    font-weight: 400;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+  `;
---
+++ src/styles/theme/default.ts
@@ -0,0 +1,14 @@
+export const defaultTheme = {
+	colors: {
+		"base-background": "#E2E8F0",
+		"base-primary": "#0069CA",
+		"base-secondary": "#8C19D2",
+		"title-profile": "#171923",
+		"description-profile": "#4A5568",
+	},
+
+	fonts: {
+		title: "'Nunito'",
+		regular: "'Inter'",
+	},
+};
