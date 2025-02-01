aac00d7b821e636e2b4f81ea5a0fb4014fec4a4b
Lucas Eduardo <lucaseduardo@MacBook-Pro-de-Lucas.local>
2025-02-01 13:30:08 -0300

    teste

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,37 +5,37 @@ import IconSearch from "../../assets/icon-search.svg";
 import { FormEvent, useState } from "react";
 
 export function Home() {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const [user, setUser] = useState("");
+  const [user, setUser] = useState("");
 
-	function handleSubmit(event: FormEvent<HTMLFormElement>) {
-		event.preventDefault();
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
 
-		navigate("/profile", {
-			state: user,
-		});
-	}
+    navigate("/profile", {
+      state: user,
+    });
+  }
 
-	return (
-		<Container>
-			<Wrapper>
-				<LogoContainer>
-					<h1 className="title-search">Search</h1>
-					<h1 className="title-dev">d_evs</h1>
-				</LogoContainer>
-				<FormSearch onSubmit={handleSubmit}>
-					<img className="icon-search" src={IconSearch} />
-					<input
-						value={user}
-						onChange={({ target }) => setUser(target.value)}
-						placeholder="Search"
-					></input>
-					<button className="button-search" type="submit">
-						Search
-					</button>
-				</FormSearch>
-			</Wrapper>
-		</Container>
-	);
+  return (
+    <Container>
+      <Wrapper>
+        <LogoContainer>
+          <h1 className="title-search">Search</h1>
+          <h1 className="title-dev">d_evs</h1>
+        </LogoContainer>
+        <FormSearch onSubmit={handleSubmit}>
+          <img className="icon-search" src={IconSearch} />
+          <input
+            value={user}
+            onChange={({ target }) => setUser(target.value)}
+            placeholder="Search"
+          ></input>
+          <button className="button-search" type="submit">
+            Search
+          </button>
+        </FormSearch>
+      </Wrapper>
+    </Container>
+  );
 }
